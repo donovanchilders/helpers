1cde2f61ca3601a3bf420f120f0d6a4b718421d5
Donovan Childers <tsound@hotmail.com>
2019-04-25 14:22:21 -0700

    Remove example script.

--- JavaScript/AnyArrayNeedlesInHaystack/any-array-needles-in-haystack.js
+++ JavaScript/AnyArrayNeedlesInHaystack/any-array-needles-in-haystack.js
@@ -1,13 +1,8 @@
 "use strict";
 
-var needle = ['"', '[', ']'];
-var haystack = 'This is a [test]';
-
 function anyArrayNeedlesInHaystack(needles, haystack) {
     var result = needles.some(function(substring) {
         return haystack.includes(substring);
     });
     return result;
 }
-
-console.log(result);
